16b53158e6bef73857b73015624e1e1af0bb2909
Osmar <bibliotecaosmar@gmail.com>
2020-01-27 10:41:03 -0300

    Add method of magiccutter that seek match phrases.

--- highlighter_pen/magiccutter.py
+++ highlighter_pen/magiccutter.py
@@ -1,23 +1,35 @@
 import re
 
 class MagicCutter:
-  phrases = []
+  def __init__(self, word = None, text = None):
+    self.__text = text
+    self.__word = self.find_word(word)
+    self.__phrases = self.find_phrases()
 
-  def __init__(self, text):
-    self.text = text
+  def text(self):
+    return self.__text
 
-  def find(self, word):
-    self.word = re.search(word, self.text)
+  def word(self):
+    return self.__word.group
+  
+  def phrases(self):
+    return self.__phrases
 
-  def phrase(self):
+  def find_word(self, word):
+    return re.search(word, self.__text)
+
+  def find_phrases(self):
+    phrases = []
     splitted_phrases = re.compile('[A-Z]?.*?\.').finditer(self.text)
 
     for phrase in splitted_phrases:
       if(
-        phrase.span()[0] <= self.word.span()[0] and
-        phrase.span()[1] >= self.word.span()[1]
+        phrase.span()[0] <= self.__word.span()[0] and
+        phrase.span()[1] >= self.__word.span()[1]
       ):
-        self.phrases.append(phrase.group()) 
+        phrases.append(phrase.group())
+        
+    return phrases
 
   def title(self):
     pass
